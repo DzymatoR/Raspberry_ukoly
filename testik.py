c1bf9f64e4d091d751e9bbca15e11dfdbd6efda9
Dzymator <dzymator@gmail.com>
2025-10-14 18:46:52 +0200

    blikani zarovky

---
+++ testik.py
@@ -0,0 +1,22 @@
+from gpiozero import LED
+from signal import pause
+from time import sleep
+
+# LED připojena na pin BCM 14 (fyzický pin 8)
+led = LED(14)
+
+print("Test LED – připojena na pin 14 (BCM).")
+print("Blikám každou sekundu. Ukonči pomocí Ctrl+C.\n")
+
+try:
+    while True:
+        led.on()
+        print("LED ON 💡")
+        sleep(1)
+        led.off()
+        print("LED OFF 💤")
+        sleep(1)
+
+except KeyboardInterrupt:
+    led.off()
+    print("\nTest ukončen. LED vypnuta bezpečně.")
